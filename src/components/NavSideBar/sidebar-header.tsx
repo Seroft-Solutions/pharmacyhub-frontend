ab476ffb738c860eca33deaa62e5cac7d84df7ba
Abdul Rehman <abdulrehmantahir353@gmail.com>
2024-12-31 14:36:38 +0500

    PH-49 Create a dashboard page

---
+++ src/components/NavSideBar/sidebar-header.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import * as React from "react"
+import { ChevronsUpDown, Plus } from "lucide-react"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar"
+
+export function SideBarHeader({
+                               teams,
+                             }: {
+  teams: {
+    name: string
+    logo: React.ElementType
+    plan: string
+  }[]
+}) {
+  const { isMobile } = useSidebar()
+  const [activeTeam, setActiveTeam] = React.useState(teams[0])
+
+  return (
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+                  <img
+                    src="/Images/PharmacyHub.png"
+                    alt="Pharmacy Hub logo"
+                    className="size 4"
+                    />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  Pharmacy Hub
+                </span>
+                  <span className="truncate text-xs">{activeTeam.plan}</span>
+                </div>
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+  )
+}
