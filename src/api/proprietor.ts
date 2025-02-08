857c708b4daeb713ba3febb43191831a32486d0d
Abdul Rehman <abdulrehmantahir353@gmail.com>
2025-02-08 18:48:43 +0500

    PH-78-propriter-related-chnages-to-connect-with-other-users

--- src/api/proprietor.ts
+++ src/api/proprietor.ts
@@ -1,7 +1,5 @@
 import {useApi} from '@/hooks/useApi';
 import {useAuth} from '@/hooks/useAuth';
-import {PharmacistVO} from "@/api/pharmacist";
-
 export interface ProprietorDetails {
   firstName: string;
   lastName: string;
@@ -9,7 +7,7 @@ export interface ProprietorDetails {
   proprietor: ProprietorVO;
 }
 export interface ProprietorVO {
-  id: string;
+  id: number;
   licenseRequired: string;
   requiredLicenseDuration: string;
   pharmacyName: string;
@@ -18,17 +16,40 @@ export interface ProprietorVO {
   contactNumber: string;
 
 }
-
+export interface ProprietorsConnectionsDTO {
+  proprietorId: number;
+  userId?: string;
+  connectionStatusEnum?: string;
+  notes?: string;
+  userGroup?: string;
+}
 export const useProprietorApi = () => {
   const { getToken, logout } = useAuth();
   const { get, post, put, del } = useApi(getToken, logout);
 
   const getProprietor = () => get<ProprietorDetails[]>('/api/proprietor/v1/get-all');
   const AddUserInProprietorGroup=(data)=>post<ProprietorVO[]>(`/api/proprietor/v1/add-info`, data);
+  const connectWithProprietor = (data: ProprietorsConnectionsDTO) =>
+      post('/api/proprietor/v1/connect', data);
+
+  const approveStatus = (id) =>
+      post(`/api/proprietor/v1/approveStatus/${id}`);
+
+  const rejectStatus = (id) =>
+      post(`/api/proprietor/v1/rejectStatus/${id}`);
+  const getProprietorRequests = () => get<ProprietorDetails[]>('/api/proprietor/v1/get-all-pending-requests');
+
+  const getAllConnections = () => get<ProprietorDetails[]>('/api/proprietor/v1/get-all-connections');
+
 
 
   return {
     getProprietor,
     AddUserInProprietorGroup,
+    getAllConnections,
+    getProprietorRequests,
+    approveStatus,
+    rejectStatus,
+    connectWithProprietor
   };
 };
