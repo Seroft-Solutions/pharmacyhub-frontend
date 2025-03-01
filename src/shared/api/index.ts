5fecae30cf27ae4b23bf3a77cf41a5436b9899af
Syed Usman <63198671+syedus06@users.noreply.github.com>
2025-03-01 22:47:40 +0500

    Remove api from shared

--- src/shared/api/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Centralized API exports
- */
-
-export * from './types';
-export { apiClient, createApiClient } from './apiClient';
-export { tokenManager, createTokenManager } from './tokenManager';
